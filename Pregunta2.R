4ea3df0dd75f757c674a7a69d46f0f94c9247b1e
Arnauupc <arnau.casanovas@estudiantat.upc.edu>
2024-10-31 11:38:22 +0100

    Update Pregunta2.R

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,23 @@
 
+esperat <- 10000
+lambda <- 1/esperat
+lambda
+
+# P(X>= 13000)
+1-pexp(13000,lambda)
+
+# probabilitat que les components superin el punt on 
+# 0.8 = F(t)= P(T<= t)
+# F ^-1(0.8)=t
+qexp(0.8, lambda)
+
+
+set.seed(85)
+simulacion <- rexp(100,lambda)
+
+mean(simulacion)
+median(simulacion)
+var(simulacion)
+#lhem de fer sempre en ordre des de set seedd
+
+
